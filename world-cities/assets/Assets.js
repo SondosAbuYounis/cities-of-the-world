bc63cd18ccf720e24f470efcabcd6f09a0ba2784
SondosAbuYounis <abuyounissundos@gmail.com>
2023-10-24 01:09:44 +0300

    Set the development invironment

---
+++ world-cities/assets/Assets.js
@@ -0,0 +1,1 @@
+import newyorkcity from './newyorkcity.jpg'
---
+++ world-cities/constants/CitiesData.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export const citiesData = [
+    {
+        id: 1,
+        name: "New York City",
+        country: "United States",
+        info: "New yorkers say: It's the city of dreams!",
+        image: ""
+    },
+    {
+        id: 2,
+        name: "Chicago",
+        country: "United States",
+        info: "",
+        image: ""
+    },
+    {
+        id: 3,
+        name: "Savannah",
+        country: "United States",
+        info: "",
+        image: ""
+    },
+    {
+        id: 4,
+        name: "Milan",
+        country: "Italy",
+        info: "",
+        image: ""
+    },
+    {
+        id: 5,
+        name: "Venice",
+        country: "Italy",
+        info: "",
+        image: ""
+    },
+    {
+        id: 6,
+        name: "Pisa",
+        country: "Italy",
+        info: "",
+        image: ""
+    },
+    {
+        id: 7,
+        name: "Tokyo",
+        country: "Japan",
+        info: "",
+        image: ""
+    },
+    {
+        id: 8,
+        name: "",
+        country: "Japan",
+        info: "",
+        image: ""
+    },
+    {
+        id: 9,
+        name: "",
+        country: "Japan",
+        info: "",
+        image: ""
+    }
+]
